d04f52b4325b5faf758aaa95aa25b43bddbe4bed
Alex Clay <alex@suran.com>
2017-08-25 15:53:13 -0400

    Use latest salt

--- script/cmtool.bat
+++ script/cmtool.bat
@@ -139,10 +139,10 @@ goto exit0
 :salt
 ::::::::::::
 
-if "%CM_VERSION%" == "latest" set CM_VERSION=2016.11.3
+if "%CM_VERSION%" == "latest" set CM_VERSION=2017.7.1
 
-if not defined SALT_64_URL set SALT_64_URL=https://repo.saltstack.com/windows/Salt-Minion-%CM_VERSION%-AMD64-Setup.exe
-if not defined SALT_32_URL set SALT_32_URL=https://repo.saltstack.com/windows/Salt-Minion-%CM_VERSION%-x86-Setup.exe
+if not defined SALT_64_URL set SALT_64_URL=https://repo.saltstack.com/windows/Salt-Minion-%CM_VERSION%-Py2-AMD64-Setup.exe
+if not defined SALT_32_URL set SALT_32_URL=https://repo.saltstack.com/windows/Salt-Minion-%CM-VERSION%-x86-Setup.exe
 
 if defined ProgramFiles(x86) (
   set SALT_URL=%SALT_64_URL%
