95a27a99284ecbc4f2493675fc03ebe38deab99f
Alex Clay <aclay@mac.com>
2018-02-08 22:58:51 -0500

    Pre-bake git

--- script/cmtool.bat
+++ script/cmtool.bat
@@ -177,6 +177,9 @@ ver>nul
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 @if errorlevel 1 echo ==^> WARNING: Error %ERRORLEVEL% was returned installing chocolatey
 
+:: Install git
+powershell.exe "choco install git --yes"
+
 goto exit0
 
 ::::::::::::
