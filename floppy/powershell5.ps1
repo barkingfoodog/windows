ebb83150ec842528e7a8509ff64ce7d09db3e0da
Alex Clay <aclay@mac.com>
2018-05-02 17:30:38 -0400

    Attempting to fix powershell 5 install

--- floppy/powershell5.ps1
+++ floppy/powershell5.ps1
@@ -8,10 +8,14 @@ if ($PSVersionTable.PSVersion.Major -ge 5) {
 if (!(Get-ItemProperty -Path 'HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full' -ErrorAction SilentlyContinue).Version -like '4.5*') {
 	choco install dotnet4.5 -y
 
-    # Restart
-    Restart-Computer
     exit 0
 }
 
-# Install PowerShell
-choco install powershell -y
+# Require pstools
+if (!(Get-Command psexec)) {
+    choco install pstools -y
+}
+
+# Install PowerShell over psexec
+# See https://serverfault.com/questions/559287/what-does-wusa-exe-return-code-5-mean
+psexec.exe -accepteula -u vagrant -p vagrant -h -i 1 powershell.exe -command "choco install powershell -y"
